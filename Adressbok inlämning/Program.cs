c5d146067d8de0dd1beffb813a4c90bd29f3b235
anthony tran <anth.tran00@gmail.com>
2020-11-06 12:29:14 +0100

    Add project files.

---
+++ Adressbok inlämning/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Adressbok_inlämning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Anthony\Adressfil.txt";
+
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

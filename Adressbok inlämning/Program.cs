0bed83f7e805c29a29dbb6b2dbdcc73e3b0cc02c
anthony tran <anth.tran00@gmail.com>
2020-11-06 13:51:08 +0100

    Entry function

--- Adressbok inlämning/Program.cs
+++ Adressbok inlämning/Program.cs
@@ -13,25 +13,61 @@ namespace Adressbok_inlämning
         {
             string filePath = @"C:\Users\Anthony\Adressfil.txt";
 
+            List<Person> people = new List<Person>();
+
             List<string> lines = File.ReadAllLines(filePath).ToList();
 
-            foreach (string line in lines)
+            try
             {
-                Console.WriteLine(line);
+                foreach (var line in lines)
+                {
+                    string[] entries = line.Split(',');
+
+                    Person newPerson = new Person();
+
+                    newPerson.Name = entries[0];
+                    newPerson.Adress = entries[1];
+                    newPerson.Phone = entries[2];
+                    newPerson.Email = entries[3];
+
+
+
+                    people.Add(newPerson);
+                }
+            }
+            catch { }
+
+            Console.WriteLine("Läst från fil");
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Name}\n" +
+                                  $"{person.Adress}\n" +
+                                  $"{person.Phone}\n" +
+                                  $"{person.Email} ");
             }
 
-            lines.Add(Console.ReadLine());
+            people.Add(new Person { Name = Console.ReadLine(), Adress = Console.ReadLine(), Phone = Console.ReadLine(), Email = Console.ReadLine() });
 
-            File.WriteAllLines(filePath, lines);
+            List<string> output = new List<string>();
 
-            Console.ReadLine();
+            foreach (var person in people)
+            {
+                output.Add($"{ person.Name }, { person.Adress }, { person.Phone },{ person.Email } ");
+            }
+
+            Console.WriteLine("Skriver till fil");
+
+            File.WriteAllLines(filePath, output);
+
+            Console.WriteLine("Alla poster inskrivna");
+
+            Console.ReadKey();
         }
     }
 
     public class Person
     {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        public string Name { get; set; }
         public string Adress { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }

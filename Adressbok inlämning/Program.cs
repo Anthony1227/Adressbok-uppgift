e9b6f502e3e209f19228fb40205515134d7874c3
anthony tran <anth.tran00@gmail.com>
2020-11-06 12:42:34 +0100

    Created a new class

--- Adressbok inlämning/Program.cs
+++ Adressbok inlämning/Program.cs
@@ -15,12 +15,26 @@ namespace Adressbok_inlämning
 
             List<string> lines = File.ReadAllLines(filePath).ToList();
 
-            foreach(string line in lines)
+            foreach (string line in lines)
             {
                 Console.WriteLine(line);
             }
 
+            lines.Add(Console.ReadLine());
+
+            File.WriteAllLines(filePath, lines);
+
             Console.ReadLine();
         }
+    }
+
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
     }
 }
